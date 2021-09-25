77442faa391a4a44f56f9f0a39b4aa9248ef70e7
Alaa Eddin Koraiem <alaadahmed@ohotnik.local>
2021-09-25 09:33:44 +0200

    Third update

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,6 +1,11 @@
-addEventListener("fetch", (event) => {
-	const response = new Response("Hello World one more time!", {
-		headers: { "content-type": "text/plain" },
-	});
-	event.respondWith(response)
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World third try!";
 });
+
+await app.listen({ port: 8000 });
+
+addEventListener("fetch", app.fetchEventHandler());
