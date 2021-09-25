b41d4f77bc5e18d71f53558cd9c5c39b996e29dc
Alaa Eddin Koraiem <alaadahmed@ohotnik.local>
2021-09-25 11:01:45 +0200

    messages

--- deno-chat-client/pages/index.tsx
+++ deno-chat-client/pages/index.tsx
@@ -1,28 +1,49 @@
 /** @jsx h */
-import { h, IS_BROWSER, useState, useEffect, useCallback} from "../deps.ts";
+import { h, IS_BROWSER, useCallback, useEffect, useState } from "../deps.ts";
 
 interface Message {
-	text: string;
+  text: string;
 }
 
 export default function Home() {
-	const [messages, setMessages] = useState<Message[]>([])
-	const getMessages = useCallback(
-		async () => {
-			const res = await fetch('https://alga-deno-chat-api.deno.dev/messages')
-			const data = await res.json()
-			setMessages(data)
-		},
-		[],
-	)
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [text, setText] = useState("");
+  const getMessages = useCallback(
+    async () => {
+      const res = await fetch("https://alga-deno-chat-api.deno.dev/messages");
+      const data = await res.json();
+      setMessages(data);
+    },
+    [],
+  );
 
-	useEffect(() => {
-		getMessages();
-	}, [])
+  useEffect(() => {
+    getMessages();
+  }, []);
 
+  const onSendMessage = useCallback(
+    async () => {
+      await fetch("https://alga-deno-chat-api.deno.dev/messages", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+      });
+      setText("");
+      getMessages();
+    },
+    [text],
+  );
   return (
     <div>
-	    {JSON.stringify(messages)}
+      <div>{JSON.stringify(messages)}</div>
+      <input
+        type="text"
+        value={text}
+        onChange={(evt) => setText(evt.target.value)}
+      />
+      <button onClick={onSendMessage}>Add</button>
     </div>
   );
 }
