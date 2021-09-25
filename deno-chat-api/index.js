361fb6751624fe3832daae7e0a63391d008c5597
Alaa Eddin Koraiem <alaadahmed@ohotnik.local>
2021-09-25 09:29:08 +0200

    second update

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-	const response = new Response("Hello World!", {
+	const response = new Response("Hello World one more time!", {
 		headers: { "content-type": "text/plain" },
 	});
 	event.respondWith(response)
